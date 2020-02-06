e78eff48bf88bb932dce9c92f84ec03b61ce696b
sujanodari <odarisujan@gmail.com>
2020-02-06 14:46:25 +0545

    [#2] Insalling packages and installizing react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+//rendering App component
+
+ReactDOM.render(<h1>sujan</h1>,document.getElementById("root"));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const path = require("path");
+
+
+module.exports={
+
+    devtool:"inline-source-map",
+    entry:{
+        "index":"./src/index.js"
+    },
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:"index.js"
+    },
+
+    module :{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:"babel-loader"
+            },
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
+            }
+        ]
+    },
+
+    devServer:{
+        contentBase:path.join(__dirname,"public/"),
+        port:3000,
+        hot:true,
+        historyApiFallback:true,
+        publicPath:"http://localhost:3000/dist/"
+    }
+}
