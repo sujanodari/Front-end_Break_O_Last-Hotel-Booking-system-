5430f747b54a39fcf5b9d2ee5b3be84ba98955d3
sujanodari <odarisujan@gmail.com>
2020-02-12 21:20:47 +0545

    [#10] fetching user deatail profile image and updating user password

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,13 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import RoomList from './RoomList';
 import Navigation from './Navigation';
 import Footer from './Footer';
-import {
-    Nav,
-    Container,
-    Col,
-    Row,
-    Table
-} from 'react-bootstrap'
 import Axios from 'axios';
 class Dashboard extends React.Component{
 
@@ -33,44 +26,15 @@ class Dashboard extends React.Component{
             }).catch((err)=>{
                console.log(err)
             })
-
-         
-            // Axios.get('http://localhost:3012/api/v1/users', this.state, this.state.config)
-            // .then((response) => {
-            //   //  console.log(response.data)
-            //     this.setState({
-            //         user_id: response.data._id
-            //     })
-            // })
-
     }
 
-    Footer = () => {
-        return (
-            <div className="p-3 mb-2 bg-light">
-                <Row>
-                    <Col sm = {6}>
-                        Contact us: dsa
-                    </Col>
-                    <Col sm = {6}>
-                        dsakhkjsda
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
 
     render(){
         return(
             <div>
-
-            {/* <Container>
-            <BookList room={this.state.rooms}/>
-            </Container> */}
-            
-
               <React.Fragment>
                 <Navigation />
+                <h2>You can book rooms here</h2>
                 <RoomList rooms={this.state.rooms}/>
             </React.Fragment>
             <Footer />
